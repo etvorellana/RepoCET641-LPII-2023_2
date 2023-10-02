17b4825ed80a416074f200da2ae0e58e3ff0b697
Grupo01LP2 <144719288+Grupo01LP2@users.noreply.github.com>
2023-10-02 13:47:31 -0300

    Add files via upload

---
+++ Equipe-01/Q9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int pot(int x, int n);
+
+
+int main() {
+    int x, n;
+    
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+    
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+    
+    int resultado = pot(x, n);
+    
+    printf("%d elevado a %d é igual a %d\n", x, n, resultado);
+    
+    return 0;
+}
+
+int pot(int x, int n) {
+  
+    if(n<0) exit(1);
+    if (n == 0) {
+        return 1;
+    }
+    
+    return x * pot(x, n - 1);
+}
