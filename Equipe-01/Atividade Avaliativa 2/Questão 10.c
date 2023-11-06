a4f10ce0c01f0930d5b8fc47de13d828e5166472
Tauntz233 <116987027+Tauntz233@users.noreply.github.com>
2023-11-06 14:00:28 -0300

    Correção

--- Equipe-01/Atividade Avaliativa 2/Questão 10.c
+++ Equipe-01/Atividade Avaliativa 2/Questão 10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct livro
 {
@@ -27,284 +28,325 @@ typedef struct lista
     int qtd;
 } Lista;
 
-No* criarNo(Livro livro);
-int adicionaLivroNoFinal(Lista *lista, Livro livro);
-int adicionaLivroNoInicio(Lista *lista, Livro livro);
-int adicionarNoMeio(Lista *lista, Livro livro, int pos);
-void printLista(Lista *lista);
-Livro removerNoInicio(Lista *lista);
-Livro removerNoFinal(Lista *lista);
-Livro removerNoMeio(Lista *lista, int pos);
-void removeEnter (char *str);
-void printLivro(Livro info);
+Livro criarLivro()
+{
+  fflush(stdin);
+  Livro temp;
+  printf("\n\nDigite o titulo: ");
+  fgets(temp.titulo, 50, stdin);
+  temp.titulo[strlen(temp.titulo) - 1] = '\0';
+  printf("Digite o autor: ");
+  fgets(temp.autor, 50, stdin);
+  temp.autor[strlen(temp.autor) - 1] = '\0';
+  printf("Digite o ano: ");
+  scanf("%d", &temp.ano);
+  printf("Digite a quantidade de páginas: ");
+  scanf("%d", &temp.paginas);
+  printf("Digite o preço: ");
+  scanf("%f", &temp.preco);
+  getchar();
+  printf("\n\n");
+  return temp;
+}
+
+No* criarNo(Livro livro)
+{
+  No* temp;
+  temp = (No*) malloc(sizeof(No));
+  temp->info = livro;
+  temp->prox = NULL;
+  return temp;
+}
+
+int adicionaLivroNoFinal(Lista *lista, Livro livro)
+{
+  No* aux;
+  aux = criarNo(livro);
+  if(aux == NULL)
+    return 0;
+  if(lista->qtd == 0){
+    lista->inicio = aux;
+    lista->fim = aux;
+  }
+  else if (lista->qtd == 1){
+    lista->inicio->prox = aux;
+    lista->fim = aux;
+  }
+  else {
+    lista->fim->prox = aux;
+    lista->fim = aux;
+  }
+  if (lista->fim == aux)
+    lista->qtd++;
+  return (lista->fim == aux);
+}
+
+
+int adicionaLivroNoInicio(Lista *lista, Livro livro)
+{
+  if(lista->qtd == 0)
+     return adicionaLivroNoFinal(lista, livro);
+  No* aux;
+  aux = criarNo(livro);
+  if(aux == NULL)
+    return 0;
+  aux->prox = lista->inicio;
+  lista->inicio = aux;
+  if(lista->inicio == aux)
+    lista->qtd++;
+  return (lista->inicio == aux);
+}
+
+
+int adicionarNoMeio(Lista *lista, Livro livro, int pos)
+{
+  No* aux;
+  aux = criarNo(livro);
+  if(aux == NULL || pos < 1)
+    return 0;
+  No *ant;
+  ant = lista->inicio;
+  if (lista->qtd < pos)
+    return  adicionaLivroNoFinal(lista, livro);
+  else {
+    for(int i = 1; i < (pos - 1); i++)
+      ant = ant->prox;
+    aux->prox = ant->prox;
+    ant->prox = aux;
+  }
+  if(ant->prox == aux)
+    lista->qtd++;
+  return (ant->prox == aux);
+}
+
+void printLivro(Livro livro)
+{
+  printf("Nome: %s\n", livro.titulo);
+  printf("Autor: %s\n", livro.autor);
+  printf("Ano: %d\n", livro.ano);
+  printf("Páginas: %d\n", livro.paginas);
+  printf("Preço: %.2f\n\n", livro.preco);
+}
+
+void printLista(Lista lista)
+{
+  int i = 1;
+  for(No* aux = lista.inicio; aux != NULL; aux = aux->prox, i++){
+    printf("\nLivro %d/%d\n",i, lista.qtd);
+    printLivro(aux->info);
+  }
+  printf("\n---Fim da lista---\nQuantidade de livros: %d\n\n",lista.qtd);
+}
+
+Livro removerNoInicio(Lista *lista)
+{
+  Livro removido = {0,0,0,0,0};
+  No* ffree;
+  if (lista->qtd == 0)
+    return removido;
+  
+  if (lista->qtd == 1){
+    removido = lista->inicio->info;
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd--;
+    return removido;
+  }
+  removido = lista->inicio->info;
+  ffree = lista->inicio;
+  lista->inicio = lista->inicio->prox;
+  lista->qtd--;
+  free(ffree);
+  return removido;
+}
+
+Livro removerNoFinal(Lista *lista)
+{
+  Livro removido;
+  No* ffree;
+  if(lista->qtd < 2){
+    return removerNoInicio(lista);
+  }
+  removido = lista->fim->info;
+  ffree = lista->fim;
+  No* ant;
+  for(ant = lista->inicio; ant->prox->prox != NULL; ant = ant->prox);
+  ant->prox = NULL;
+  lista->fim = ant;
+  lista->qtd--;
+  free(ffree);
+  return removido;
+}
+
+Livro removerNoMeio(Lista *lista, int pos)
+{
+  Livro removido = {0,0,0,0,0};
+  No *ant, *aux;
+  ant = lista->inicio;
+  if(lista->qtd < pos)
+    return removido;
+    
+  else if(pos == 1)
+    return removerNoInicio(lista);
+  for(int i = 1; i < (pos - 1) && ant != NULL; i++, ant = ant->prox);
+  removido = ant->prox->info;
+  aux = ant->prox;
+  ant->prox = ant->prox->prox;
+  lista->qtd--;
+  free(aux);
+  return removido;
+}
 
 int main(int argc, char const *argv[])
 {
-  int resp = 0, pos;
-  Livro aux;
   Lista meusLivros;
   meusLivros.inicio = NULL;
   meusLivros.fim = NULL;
   meusLivros.qtd = 0;
 
-  while(resp!=8){
-    printf("\n1 - Mostrar a lista\n2 - Adicionar livro no início\n3 - Adicionar livro ao meio\n4 - Adicionar livro ao final\n5 - Remover livro do início\n6 - Remover livro do meio\n7 - Remover livro do final\n8 - Encerrar o programa\n\nDigite a sua opção: ");
-    scanf("%d",&resp);
-    fflush(stdin);
-    
-    if(resp == 1) printLista(&meusLivros);
-    else if(resp == 2 || resp == 3 || resp == 4){
+  int index, sindex;
+  Livro removido;
+  while(1){
+    do {
+      printf("\n>>> Menu Principal <<<\nDigite uma opção: \n");
+      printf("1 - Adicionar;\n2 - Remover;\n3 - Consultar;\n4 - Sair\n-> ");
+      scanf("%d", &index);
       getchar();
-      printf("\nDigite o título do livro: ");
-      fgets(aux.titulo,50,stdin);
-      fflush(stdin);
-      removeEnter(aux.titulo);
-      printf("Digite o autor(a) do livro: ");
-      fgets(aux.autor,50,stdin);
-      fflush(stdin);
-      removeEnter(aux.autor);
-      printf("Digite o ano do livro: ");
-      scanf("%d", &aux.ano);
-      printf("Digite o número de páginas do livro: ");
-      scanf("%d", &aux.paginas);
-      printf("Digite o preço do livro: ");
-      scanf("%f", &aux.preco);
-      if(resp == 3){
-        printf("Digite a posição onde deseja adicionar o livro: ");
-        scanf(" %d", &pos);
-      }
-      if(resp == 2) adicionaLivroNoInicio(&meusLivros, aux);
-      if(resp == 3) adicionarNoMeio(&meusLivros, aux,pos);
-      if(resp == 4) adicionaLivroNoFinal(&meusLivros, aux);
-    }
-    else if(resp == 5 || resp == 6 || resp == 7){
-      if(resp == 5) aux = removerNoInicio(&meusLivros);
-      else if(resp == 6) {
-        printf("\nDigite a posição onde deseja remover o livro: ");
-        scanf(" %d", &pos);
-        aux= removerNoMeio(&meusLivros,pos);
-      }
-      else aux = removerNoFinal(&meusLivros);
-      if(aux.titulo[0] == 0) printf("\nErro na remoção. Tente novamente!\n");
-      else{
-        printf("\nLivro removido: \n");
-        printLivro(aux);
-      }
-    }
-    else if(resp >8 || resp < 1) printf("\nEntrada inválida, tente novamente!\n");
-    
+    } while(index < 1 || index > 4);
+    switch(index){
+      case 1:
+        do {
+          printf("\n+++ Menu Adicionar +++\nEscolha uma das opções: \n");
+          printf("1 - Adicionar no início;\n2 - Adicionar no meio;\n3 - Adicionar no fim;\n4 - Voltar\n-> ");
+          scanf("%d", &sindex);
+        } while(sindex < 1 || sindex > 4);
+        getchar();
+        switch(sindex) {
+          case 1:
+            if(adicionaLivroNoInicio(&meusLivros, criarLivro()) == 0)
+              printf("!!!!!!!!!ERRO AO ADICIONAR!!!!!!!!!\n");
+          break;
+          case 2:
+            printf("\nQual posição?\n-> ");
+            int poa;
+            scanf("%d", &poa);
+            getchar();
+            if(poa > meusLivros.qtd + 1) {
+              printf("A posição é maior que o indíce atual de livros.\n");
+              printf("O Livro será adicionado ao final da lista na posição %d\n", meusLivros.qtd + 1);
+              printf("Continuar? S para confirmar\n-> ");
+              unsigned char conf;
+              scanf("%c", &conf);
+              getchar();
+              if(conf == 'S' || conf == 's') {
+                if(adicionaLivroNoFinal(&meusLivros, criarLivro()) == 0)
+                  printf("!!!!!!!!!ERRO AO ADICIONAR!!!!!!!!!\n");
+              }
+              else
+                  printf("Retornando ao menu.\n");
+            }
+            else if(adicionarNoMeio(&meusLivros, criarLivro(), poa) == 0)
+              printf("!!!!!!!!!ERRO AO ADICIONAR!!!!!!!!!\n");
+          break;
+          case 3:
+            if(adicionaLivroNoFinal(&meusLivros, criarLivro()) == 0)
+              printf("!!!!!!!!!ERRO AO ADICIONAR!!!!!!!!!\n");
+          break;
+          default:
+          break;
+        }
+      break;
+      case 2:
+        if(meusLivros.qtd == 0)
+          printf("Não há livros para serem removidos!\n");
+        else {
+          do {
+            printf("\n--- Menu Remoção ---\nEscolha uma das opções: \n");
+            printf("1 - Remover no início;\n2 - Remover no meio;\n3 - Remover no fim\n4+ - Voltar\n-> ");
+            scanf("%d", &sindex);
+          } while(sindex < 1);
+          getchar();
+          switch(sindex) {
+            case 1:
+              removido = removerNoInicio(&meusLivros);
+              printf("\nLivro Removido: \nLivro 1\n");
+              printLivro(removido);
+            break;
+            case 2:
+              printf("\nQual posição?\n-> ");
+              int por;
+              do {
+                scanf("%d", &por);
+                getchar();
+                if(por < 1)
+                  printf("Valor inválido!\nDigite novamente a posição: ");
+              } while (por < 1 || por > meusLivros.qtd);
+              removido = removerNoMeio(&meusLivros, por);
+              printf("\nLivro Removido: \nLivro %d\n",por);
+              printLivro(removido);
+            break;
+            case 3:
+              removido = removerNoFinal(&meusLivros);
+              printf("Livro Removido: \n");
+              printLivro(removido);
+            break;
+            default:
+            break;
+          }
+        }
+      break;
+      case 3:
+        if(meusLivros.inicio == NULL)
+          printf("A lista não possui livros!\n");
+        else {
+          do {
+            printf("\n<><> Menu de Visualização <><>\nEscolha uma das opções: \n");
+            printf("1 - Ver lista completa;\n2 - Ver livro inicial;\n3 - Ver livro final\n4 - Ver livro específico\n5 - Voltar\n-> ");
+            scanf("%d", &sindex);
+          } while(sindex < 1 || sindex > 5);
+          getchar();
+          switch(sindex) {
+            case 1:
+              printLista(meusLivros);
+            break;
+            case 2:
+              printLivro(meusLivros.inicio->info);
+            break;
+            case 3:
+              printLivro(meusLivros.fim->info);
+            break;
+            case 4:
+              printf("Qual posição?\n-> ");
+              int pov;
+              do {
+                scanf("%d", &pov);
+                getchar();
+                if(pov > meusLivros.qtd)
+                  printf("A quantidade de livros é menor que a posição\nDigite Novamente: ");
+                else if (pov < 1)
+                  printf("O indice não existe.\nDigite Novamente: ");
+              } while (pov > meusLivros.qtd || pov < 1);
+              if(pov == 1)
+                printLivro(meusLivros.inicio->info);
+              else if (pov == meusLivros.qtd)
+                printLivro(meusLivros.fim->info);
+              else {
+                No* a;
+                a = meusLivros.inicio;
+                for(int i = 1; i < pov; i++)
+                  a = a -> prox;
+                printLivro(a->info);
+              }
+            break;
+            default:
+            break;
+          }
+        }
+      break;
+      default:
+        return 0;
+      break; 
   }
-  
-  return 0;
-}
-
-/*
-    Função: criarNo
-    Objetivo: Criar um novo nó para a lista
-    Parâmetros: Livro livro - livro a ser inserido no nó
-    Retorno: No* - ponteiro para o nó criado
-*/
-No* criarNo(Livro livro)
-{
-  No* noh = (No*) malloc(sizeof(No));
-  noh->info = livro;
-  noh->prox = NULL;
-  
-  return noh;
-}
-
-/*
-    Função: adicionarNo
-    Objetivo: Adicionar um nó n final da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-                No *no - ponteiro para o nó a ser adicionado
-    Retorno: int - 1 se conseguiu adicionar o nó, 0 caso contrário
-*/
-
-int adicionaLivroNoFinal(Lista *lista, Livro livro)
-{
-  No* liv = criarNo(livro);
-  if(liv == NULL) return 0;
-  
-  if(lista->inicio == NULL){
-    lista->inicio = lista->fim = liv;
   }
-  else {
-    lista->fim->prox = liv;
-    lista->fim = liv;
-  }
-
-  lista->qtd++;
   return 1;
-}
-
-/*
-    Função: adicionarNoInicio
-    Objetivo: Adicionar um nó no início da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-                No *no - ponteiro para o nó a ser adicionado
-    Retorno: int - 1 se conseguiu adicionar o nó, 0 caso contrário
-*/
-int adicionaLivroNoInicio(Lista *lista, Livro livro)
-{
-  No* liv = criarNo(livro);
-  if(liv == NULL) return 0;
-
-  if(lista->inicio == NULL){
-    lista->inicio = lista->fim = liv;
-  }
-  else {
-    liv->prox = lista->inicio;
-    lista->inicio = liv;
-  }
-  
-  lista->qtd++;
-  return 1;
-}
-
-/*
-    Função: adicionarNoMeio
-    Objetivo: Adicionar um nó no meio da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-                No *no - ponteiro para o nó a ser adicionado
-                int pos - posição onde o nó será adicionado
-    Retorno: int - 1 se conseguiu adicionar o nó, 0 caso contrário
-*/
-int adicionarNoMeio(Lista *lista, Livro livro, int pos)
-{
-  No* liv = criarNo(livro);
-  if(liv == NULL || pos<1){
-    free(liv);
-    return 0;
-  }
-
-  if(lista->inicio == NULL || pos == 1){
-    adicionaLivroNoInicio(lista,livro);
-  }
-  else if(pos > lista->qtd) {
-    adicionaLivroNoFinal(lista, livro);
-  }
-  else{
-    No* aux = lista->inicio;
-    for(int i = 1; i< pos-1;i++){
-      aux = aux->prox;
-    }
-    liv->prox = aux->prox;
-    aux->prox = liv;
-  }
-
-  lista->qtd++;
-  return 1;
-}
-
-void printLista(Lista *lista)
-{
-  No* aux = lista->inicio;
-  if(lista->qtd == 0) printf("\nLista vazia!\n\n");
-  for(int i =0; i<lista->qtd; i++){
-    printf("\nTítulo: %s\nAutor: %s\nAno: %d\nPáginas: %d\nPreço: %.2f\n\n",aux->info.titulo,aux->info.autor,aux->info.ano,aux->info.paginas,aux->info.preco);
-    aux = aux->prox;
-  }
-
-  return;
-}
-
-/*
-    Função: removerNoInicio
-    Objetivo: Remover um nó do início da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-    Retorno: Retorna o livro removido
-*/
-Livro removerNoInicio(Lista *lista)
-{
-  Livro aux = {0,0,0,0,0};
-  No *noh = lista->inicio;
-  if(lista->inicio == NULL) return aux;
-  else if(lista->qtd == 1){
-    lista->inicio= NULL;
-    lista->fim = NULL;
-  }
-  else{
-    lista->inicio = lista->inicio->prox;
-  }
-  
-  aux = noh->info;
-  free(noh);
-  lista->qtd--;
-  return aux;
-}
-
-/*
-    Função: removerNoFinal
-    Objetivo: Remover um nó do final da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-    Retorno: Retorna o livro removido
-*/
-Livro removerNoFinal(Lista *lista)
-{
-  Livro aux = {0,0,0,0,0};
-  No *noh = lista->inicio;
-  if(lista->inicio == NULL) return aux;
-  else if(lista->qtd == 1){
-    aux = lista->fim->info;
-    free(lista->fim);
-    lista->inicio= NULL;
-    lista->fim = NULL;
-  }
-  else{
-    for(int i = 1; i< lista->qtd-1;i++){
-      noh = noh->prox;
-    }
-    aux = lista->fim->info;
-    free(lista->fim);
-    lista->fim = noh;
-  }
-
-  lista->qtd--;
-  return aux;
-}
-
-/*
-    Função: removerNoMeio
-    Objetivo: Remover um nó do meio da lista
-    Parâmetros: Lista *lista - ponteiro para a lista
-                int pos - posição onde o nó será removido
-    Retorno: Retorna o livro removido
-*/
-Livro removerNoMeio(Lista *lista, int pos)
-{
-  Livro aux = {0,0,0,0,0};
-  No *noh = lista->inicio, *au;
-  
-  if(pos<1) return aux;
-
-  if(lista->inicio == NULL || pos == 1){
-    return removerNoInicio(lista);
-  }
-  else if(pos >= lista->qtd) {
-    return removerNoFinal(lista);
-  }
-  else{
-    for(int i = 1; i< pos-1;i++){
-      noh = noh->prox;
-    }
-    aux = noh->prox->info;
-    au = noh->prox;
-    noh->prox = noh->prox->prox;
-    free(au);
-  }
-
-  lista->qtd--;
-  return aux;
-}
-
-void removeEnter (char *str){
-  str[strlen(str)-1] = '\0';
-  return;
-}
-
-void printLivro(Livro info)
-{
-  printf("\nTítulo: %s\nAutor: %s\nAno: %d\nPáginas: %d\nPreço: %.2f\n\n",info.titulo,info.autor,info.ano,info.paginas,info.preco);
-  return;
 }
