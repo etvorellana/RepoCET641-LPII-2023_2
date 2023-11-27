569d6cb053d44a73d5789eefd200e7e93459324b
Grupo01LP2 <144719288+Grupo01LP2@users.noreply.github.com>
2023-11-27 13:31:11 -0300

    Adição na questão

--- Equipe-01/Atividade Avaliativa 3/02.c
+++ Equipe-01/Atividade Avaliativa 3/02.c
@@ -2,19 +2,31 @@
 #include <ctype.h>
 #include <string.h>
 
+
 int main(){
   FILE *arquivo = fopen("dados.bin", "wb");
-  if (arquivo == NULL) {
-    printf("Erro ao abrir o arquivo.\n");
-    return 1;
+  
+  if(arquivo){
+    for (int i = 1; i <= 100; i++){
+      char c[5];
+      sprintf(c, "%d\n", i);
+      fwrite(&c, sizeof(char), strlen(c), arquivo);
+    }
+    fclose(arquivo);
+    printf("Arquivo binário criado com sucesso: dados.bin\n");
+    return 0;
   }
-  for (int i = 1; i <= 100; i++){
-    char c[5];
-    sprintf(c, "%d\n", i);
-    fwrite(&c, sizeof(char), strlen(c), arquivo);
-  }
-  fclose(arquivo);
-  printf("Arquivo binário criado com sucesso: dados.bin\n");
-
-  return 0;
+  /*
+  if(arquivo){
+    for(int i = 1; i <= 100; i++){
+      char c = (i == 100) ? '\0' : '\n';
+      fwrite(&i, sizeof(int), 1, arquivo);
+      fwrite(&c, sizeof(char), 1, arquivo);
+    }
+    fclose(arquivo);
+    printf("Arquivo criado com sucesso.\n");
+    return 0;
+  }*/
+  printf("Erro ao abrir o arquivo.\n");
+  return 1;
 }
